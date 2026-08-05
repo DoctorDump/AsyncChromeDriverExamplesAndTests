994b5e21191c84fe6e067e1b05e81a2e8b9640be
Oleg Fedko <olegfedko@yandex.ru>
2026-08-05 21:13:03 +0300

    CancellationToken is used to control Chrome startup timeout

--- AsyncChromeDriver.SeleniumAdapter/ChromeDriver.cs
+++ AsyncChromeDriver.SeleniumAdapter/ChromeDriver.cs
@@ -1,30 +1,31 @@
-﻿using OpenQA.Selenium;
+﻿using System.Threading;
+using OpenQA.Selenium;
 
 namespace Zu.AsyncChromeDriver.SeleniumAdapter
 {
     public class ChromeDriver: WebDriverAdapter
     {
-        public ChromeDriver()
-            : this("")
+        public ChromeDriver(CancellationToken ct)
+            : this("", ct)
         {
         }
-        public ChromeDriver(string profileName)
-            :base(profileName)
-        {
-        }
-
-        public ChromeDriver(Zu.Chrome.AsyncChromeDriver asyncChromeDriver)
-                : base(asyncChromeDriver)
+        public ChromeDriver(string profileName, CancellationToken ct)
+            :base(profileName, ct)
         {
         }
 
-        public ChromeDriver(Zu.Chrome.ChromeDriverConfig config)
-                 : base(config)
+        public ChromeDriver(Zu.Chrome.AsyncChromeDriver asyncChromeDriver, CancellationToken ct)
+                : base(asyncChromeDriver, ct)
         {
         }
 
-        public ChromeDriver(DriverOptions options)
-                  : base(options)
+        public ChromeDriver(Zu.Chrome.ChromeDriverConfig config, CancellationToken ct)
+                 : base(config, ct)
+        {
+        }
+
+        public ChromeDriver(DriverOptions options, CancellationToken ct)
+                  : base(options, ct)
         {
         }
     }
--- AsyncChromeDriver.SeleniumAdapter/WebDriverAdapter.cs
+++ AsyncChromeDriver.SeleniumAdapter/WebDriverAdapter.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using System.Threading;
+using OpenQA.Selenium;
 using Zu.SeleniumAdapter;
 
 namespace Zu.AsyncChromeDriver.SeleniumAdapter
@@ -7,33 +8,33 @@ namespace Zu.AsyncChromeDriver.SeleniumAdapter
     {
         protected Zu.Chrome.AsyncChromeDriver asyncChromeDriver;
 
-        public WebDriverAdapter()
-            : this("")
+        public WebDriverAdapter(CancellationToken ct)
+            : this("", ct)
         {
         }
-        public WebDriverAdapter(string profileName)
+        public WebDriverAdapter(string profileName, CancellationToken ct)
         {
             asyncChromeDriver = string.IsNullOrWhiteSpace(profileName) ? new Zu.Chrome.AsyncChromeDriver() : new Zu.Chrome.AsyncChromeDriver(profileName);
-            Create(asyncChromeDriver);
+            Create(asyncChromeDriver, ct);
         }
 
-        public WebDriverAdapter(Zu.Chrome.AsyncChromeDriver asyncChromeDriver)
+        public WebDriverAdapter(Zu.Chrome.AsyncChromeDriver asyncChromeDriver, CancellationToken ct)
         {
             this.asyncChromeDriver = asyncChromeDriver;
-            Create(asyncChromeDriver);
+            Create(asyncChromeDriver, ct);
         }
 
-        public WebDriverAdapter(Zu.Chrome.ChromeDriverConfig config)
+        public WebDriverAdapter(Zu.Chrome.ChromeDriverConfig config, CancellationToken ct)
         {
             this.asyncChromeDriver = new Chrome.AsyncChromeDriver(config);
-            Create(asyncChromeDriver);
+            Create(asyncChromeDriver, ct);
         }
         
-        public WebDriverAdapter(DriverOptions options)
+        public WebDriverAdapter(DriverOptions options, CancellationToken ct)
         {
             var config = ConvertDriverOptionsToChromeDriverConfig(options);
             this.asyncChromeDriver = new Chrome.AsyncChromeDriver(config);
-            Create(asyncChromeDriver);
+            Create(asyncChromeDriver, ct);
         }
 
         private Zu.Chrome.ChromeDriverConfig ConvertDriverOptionsToChromeDriverConfig(DriverOptions options)
@@ -43,11 +44,11 @@ namespace Zu.AsyncChromeDriver.SeleniumAdapter
             return res;
         }
 
-        private void Create(Chrome.AsyncChromeDriver asyncChromeDriver)
+        private void Create(Chrome.AsyncChromeDriver asyncChromeDriver, CancellationToken ct)
         {
             AsyncWebDriver = new Zu.AsyncWebDriver.Remote.WebDriver(asyncChromeDriver);
             SyncWebDriver = new Zu.AsyncWebDriver.Remote.SyncWebDriver(AsyncWebDriver);
-            SyncWebDriver.Open();
+            SyncWebDriver.Open(ct);
         }
     }
 }
